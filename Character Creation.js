11c7f496430faa2b7e8693b3a333b25d53a5c174
Cal Corum <calcorum@users.noreply.github.com>
2017-09-23 02:02:32 -0500

    Update Character Creation.js

--- Character Creation.js
+++ Character Creation.js
@@ -15,6 +15,7 @@ let attributes = ["attribute-strength", "attribute-dexterity",
 let weaponExample = "/w gm &{template:default} {{name=@{character_name} / Tactical Semi-Auto Pistol}} {{To Hit=[[1d20+2]]}} {{Damage=[[1d6]] F; critical burn [[1d4]]}} {{Effect=None}}";
 let grenadeExample = "/w gm &{template:default} {{name=@{character_name} / Name of Grenade}} {{To Hit=[[1d20-?{Range Penalty? (-2 per range increment over 30ft)|0}]]; DC 5}} {{Damage=[[1d6]] P}} {{Effect=Explode 15 ft.}} {{Scatter=[Roll Here](!#ThrownWeapon-Scatter)}}";
 let spellExample = "/w gm &{template:default} {{name=@{character_name} / Spell Name}} {{Target=one creature}} {{Duration=instantaneous}} {{Saving Throw=Will half; DC [[10+SpellLevel+floor(((@{selected|attribute-intelligence}-10)/2))]]}} {{Spell Resistance=yes}} {{Damage=[[1d10]]}} {{Description=Any extra description needed}}"
+let notes = "/w gm &{template:default} {{name=@{character_name} / Notes}}{{Alignment / Race=@{alignment} / @{race}}} {{Speed=30 ft.}} {{Languages=@{languages}}} {{Before Combat=SampleText}} {{During Combat=SampleText}} {{Morale=SampleText}} {{Gear=AllTheLoot}}"
 
 on("ready", function(){
     on("add:character", function(char){
@@ -33,17 +34,17 @@ on("ready", function(){
                 characterid: char.id
             });
         });
-        _.each(skills, function(sk){
-            createObj("attribute", {
-                name: sk,
-                current: 0,
-                characterid: char.id
-            });
-        });
         _.each(attributes, function(att){
             createObj("attribute", {
                 name: att,
                 current: 10,
+                characterid: char.id
+            });
+        });
+        _.each(skills, function(sk){
+            createObj("attribute", {
+                name: sk,
+                current: 0,
                 characterid: char.id
             });
         });
@@ -74,56 +75,22 @@ on("ready", function(){
             characterid: char.id,
         });
         createObj("ability", {
-            name: "Skills-List",
-            action: "#Skills-List",
+            name: "Skills",
+            action: "!skillChecks @{character_name}",
             istokenaction: true,
             characterid: char.id,
         });
         createObj("ability", {
             name: "Saves",
-            action: "#Saves",
+            action: "!saves @{character_name}",
+            istokenaction: true,
+            characterid: char.id,
+        });
+        createObj("ability", {
+            name: "Notes",
+            action: notes,
             istokenaction: true,
             characterid: char.id,
         });
     });
-});
-
-
-// Syntax: !addAbility [weapon, grenade] [CHARACTER NAME]
-// Example: !addAbility weapon Manticore
-on("chat:message", function(msg){
-    if(msg.type != "api") return;
-    
-    if(msg.type == "api" && msg.content.indexOf("!addAbility ") !== -1){
-        let input = msg.content.replace("!addAbility ","").split(" ");
-        let newAbility = input[0];
-        let char = findObjs({
-            _type: "character",
-            name: input[1],
-        })[0];
-        log("Found object: " + char.get("name"));
-        
-        if(newAbility == "weapon"){
-            createObj("ability", {
-                name: "Attack-Weapon-Sample",
-                action: weaponExample,
-                characterid: char.id,
-            });
-            sendChat("Helper Bot", "Added a weapon macro for" + char.get("name"));
-        }else if(newAbility == "grenade"){
-            createObj("ability", {
-                name: "Attack-Grenade-Sample",
-                action: grenadeExample,
-                characterid: char.id,
-            });
-            sendChat("Helper Bot", "Added a grenade macro for " + char.get("name"));
-        }else if(newAbility == "spell"){
-            createObj("ability", {
-                name: "Attack-Spell-Sample",
-                action: spellExample,
-                characterid: char.id,
-            });
-            sendChat("Helper Bot", "Added a spell macro for " + char.get("name"));
-        }
-    }
 });
