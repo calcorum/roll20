5ad38358089bfb4866a255b3ba29506e5a0aab68
Cal Corum <calcorum@users.noreply.github.com>
2017-09-23 01:19:03 -0500

    Update and rename NPC Skills.js to NPC Checks.js

--- NPC Checks.js
+++ NPC Checks.js
@@ -1,8 +1,6 @@
 // Syntax: !skillChecks [CHARACTER NAME]
 // Example: !skillChecks Manticore
 
-let chatIntro = "/w gm &{template:default}";
-let skHelp1 = ""
 function getOutputBySkill(skillName, currentValue){
     let outputVal = "";
     switch(skillName){
@@ -67,38 +65,75 @@ function getOutputBySkill(skillName, currentValue){
     return outputVal;
 }
 
+function getOutputBySave(saveName, currentValue){
+    let outputVal = "";
+    switch(saveName){
+        case "fortitude":
+            outputVal += "{{Fortitude=[[1d20+" + currentValue + "]]}} ";
+            break;
+        case "reflex":
+            outputVal += "{{Reflex=[[1d20+" + currentValue + "]]}} ";
+            break;
+        case "will":
+            outputVal += "{{Will=[[1d20+" + currentValue + "]]}} ";
+            break;
+    }
+    return outputVal;
+}
+
+// find character sent via API call
+function getChar(charName){
+    return findObjs({
+        _type: "character",
+        name: charName,
+    })[0];
+}
+// get attributes associated with character
+function getAttrs(charObj){
+    return findObjs({
+        _type: "attribute",
+        _characterid: charObj.get("id"),
+    });
+}
+
 on("chat:message", function(msg){
     if(msg.type != "api") return;
     
-    if(msg.type == "api" && msg.content.indexOf("!skillChecks ") !== -1){
+    if(msg.content.indexOf("!skillChecks ") !== -1){
         let input = msg.content.replace("!skillChecks ","").split(" ");
-        
-        // find character send via API call
-        let char = findObjs({
-            _type: "character",
-            name: input[0],
-        })[0];
-        
+        let char = getChar(input[0]);
         let skillString = "{{name=" + char.get("name") + "'s Skills}} ";
-        // get attributes associated with character
-        let attributes = findObjs({
-            _type: "attribute",
-            _characterid: char.get("id"),
-        });
+        let attributes = getAttrs(char);
+        
         // if attribute name starts with "skill-" and is greater than 0,
         //   add it to the skillString
         _.each(attributes, function(att){
             let attName = att.get("name");
             let attCurrent = att.get("current");
             if(attName.indexOf("skill-") !== -1 && attCurrent > 0){
-                log("Found attribute: " + attName);
                 let skillName = attName.replace("skill-","");
-                log("Modified attribute name: " + skillName);
                 skillString += getOutputBySkill(skillName, attCurrent);
             }
         });
         skillString += "{{Default=[[1d20]]}}"
         
         sendChat("character|" + char.get("id"), "/w gm &{template:default} " + skillString);
+    }else if(msg.content.indexOf("!saves ") !== -1){
+        let input = msg.content.replace("!saves ","").split(" ");
+        let char = getChar(input[0]);
+        let saveString = "{{name=" + char.get("name") + "'s Saves}} ";
+        let attributes = getAttrs(char);
+
+        // if attribute name starts with "save-" then add it to the saveString
+        _.each(attributes, function(att){
+            let attName = att.get("name");
+            let attCurrent = att.get("current");
+            if(attName.indexOf("save-") !== -1){
+                let saveName = attName.replace("save-","");
+                saveString += getOutputBySave(saveName, attCurrent);
+            }
+        });
+
+        sendChat("character|" + char.get("id"), "/w gm &{template:default} " + saveString);
     }
 });
