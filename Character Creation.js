16a82a6904a68cac04c8fb06841b9aafaef80317
Cal Corum <calcorum@users.noreply.github.com>
2017-09-23 00:01:12 -0500

    Update Character Creation.js

--- Character Creation.js
+++ Character Creation.js
@@ -9,8 +9,12 @@ let saves = ["save-fortitude", "save-reflex", "save-will"];
 let stats = ["hitpoints", "stamina", "resolve", "initiative",
     "profession00-name", "profession00-attribute", "profession01-name",
     "profession00-attribute", "languages", "alignment", "size", "race"]
-let weaponExample = "&{template:default} {{name=@{character_name} / Tactical Semi-Auto Pistol}} {{To Hit=[[1d20+2]]}} {{Damage=[[1d6]] F; critical burn [[1d4]]}} {{Effect=None}}";
-let grenadeExample = "&{template:default} {{name=@{character_name} / Name of Grenade}} {{To Hit=[[1d20-?{Range Penalty?|0}]]; DC 5}} {{Damage=[[1d6]] P}} {{Effect=Explode 15 ft.}} {{Scatter=[Roll Here](!&#13;#ThrownWeapon-Scatter)}}";
+let attributes = ["attribute-strength", "attribute-dexterity", 
+    "attribute-constitution", "attribute-intelligence",
+    "attribute-wisdom", "attribute-charisma"];
+let weaponExample = "/w gm &{template:default} {{name=@{character_name} / Tactical Semi-Auto Pistol}} {{To Hit=[[1d20+2]]}} {{Damage=[[1d6]] F; critical burn [[1d4]]}} {{Effect=None}}";
+let grenadeExample = "/w gm &{template:default} {{name=@{character_name} / Name of Grenade}} {{To Hit=[[1d20-?{Range Penalty? (-2 per range increment over 30ft)|0}]]; DC 5}} {{Damage=[[1d6]] P}} {{Effect=Explode 15 ft.}} {{Scatter=[Roll Here](!#ThrownWeapon-Scatter)}}";
+let spellExample = "/w gm &{template:default} {{name=@{character_name} / Spell Name}} {{Target=one creature}} {{Duration=instantaneous}} {{Saving Throw=Will half; DC [[10+SpellLevel+floor(((@{selected|attribute-intelligence}-10)/2))]]}} {{Spell Resistance=yes}} {{Damage=[[1d10]]}} {{Description=Any extra description needed}}"
 
 on("ready", function(){
     on("add:character", function(char){
@@ -36,16 +40,31 @@ on("ready", function(){
                 characterid: char.id
             });
         });
+        _.each(attributes, function(att){
+            createObj("attribute", {
+                name: att,
+                current: 10,
+                characterid: char.id
+            });
+        });
+        createObj("attribute", {
+                name: "npc",
+                current: "yes",
+                characterid: char.id
+            });
         createObj("ability", {
             name: "Attack-Weapon-Sample",
             action: weaponExample,
-            istokenaction: true,
             characterid: char.id,
         });
         createObj("ability", {
             name: "Attack-Grenade-Sample",
             action: grenadeExample,
-            istokenaction: true,
+            characterid: char.id,
+        });
+        createObj("ability", {
+            name: "Attack-Spell-Sample",
+            action: spellExample,
             characterid: char.id,
         });
         createObj("ability", {
@@ -98,6 +117,13 @@ on("chat:message", function(msg){
                 characterid: char.id,
             });
             sendChat("Helper Bot", "Added a grenade macro for " + char.get("name"));
+        }else if(newAbility == "spell"){
+            createObj("ability", {
+                name: "Attack-Spell-Sample",
+                action: spellExample,
+                characterid: char.id,
+            });
+            sendChat("Helper Bot", "Added a spell macro for " + char.get("name"));
         }
     }
 });
