331d1033c389ff242ad673490eed9c891fb23202
Cal Corum <calcorum@users.noreply.github.com>
2017-09-26 23:58:59 -0500

    Update Modular Weapon.js

--- Modular Weapon.js
+++ Modular Weapon.js
@@ -1,3 +1,7 @@
+let laserPistolAzimuth = {"name":"Azimuth Laser Pistol", "damage":"1d4", "critical effect":"burn", "critical effect damage": "1d4", "toHitAttribute": "dexterity"};
+let laserRifleAzimuth = {"name":"Azimuth Laser Rifle", "damage":"1d8", "critical effect":"burn", "critical effect damage": "1d6", "toHitAttribute": "dexterity"};
+let weaponList = {"laserpistolazimuth":laserPistolAzimuth, "laserrifleazimuth":laserRifleAzimuth};
+
 // find character sent via API call
 function getChar(charName){
     return findObjs({
@@ -6,44 +10,31 @@ function getChar(charName){
     })[0];
 }
 
-// Syntax: !fireweapon [CHARACTER_NAME] [WEAPON_JSON]
-// Example: !fireweapon @{character_name} {"name": "Azimuth Laser Pistol", "damage":"1d4+1", 
-//      "critical":"burn 1d4", "toHitAttribute": "dexterity","toDamageAttribute": "strength"}
+// Syntax: !fireweapon [CHARACTER_NAME] --[WEAPON]
+// Example: !skillChecks Manticore --laserpistolazimuth
 
 on("chat:message", function(msg){
     if(msg.type != "api") return;
     
     if(msg.content.indexOf("!fireweapon ") !== -1){
-        let input = msg.content.replace("!fireweapon ","");
-        let weapStart = input.indexOf("{");
-        let char = getChar(input.slice(0,weapStart-1));
-        let attributes = getAttrs(char);
-        let weapon = JSON.parse(input.slice(weapStart));
-        let damage = weapon["damage"];
-        let toHitRoll = randomInteger(20);
-        let criticalEffect = "";
+        let input = msg.content.replace("!fireweapon ","").split(" --");
+        let char = getChar(input[0]);
+        let weapon = weaponList[input[1].toLowerCase().replace(" ","").replace(",","").replace("-","")];
+        let toHitDie = randomInteger(20);
+        let toHitBonus = Math.floor((getAttrByName(char.get("id"), "attribute-" + weapon["toHitAttribute"])-10)/2);
+        let damageString = weapon["damage"];
+        let criticalString = "";
         
-        if (toHitRoll == 20){
-            if(weapon["critical"]){
-                criticalEffect = " {{Critical Effect=" + weapon["critical"] + "}}";
+        if (toHitDie == 20){
+            damageString += " + " + weapon["damage"];
+            if(weapon["critical effect damage"]){
+                criticalString += "{{Critical!=" + weapon["critical effect"] + 
+                    " [[" + weapon["critical effect damage"] + "]]}}";
             }
-            damage += " + " + damage;
         }
-        log("On the die: " + toHitRoll);
-        
-        switch(weapon["toHitAttribute"]){
-            case "strength":
-                toHitRoll += Math.floor((getAttrByName(char.get("id"), "attribute-strength")-10)/2);
-                break;
-            case "dexterity":
-                toHitRoll += Math.floor((getAttrByName(char.get("id"), "attribute-dexterity")-10)/2);
-                break;
-        }
-        
-        log("Total roll: " + toHitRoll);
         
         sendChat("character|" + char.get("id"), "/w gm &{template:default} {{name=" + char.get("name") + " / " + 
-            weapon["name"] + "}} {{To Hit=[[" + toHitRoll + "]]}} {{Damage=[[" + damage + 
-            "]]}}" + criticalEffect);
+            weapon["name"] + "}} {{To Hit=[[" + toHitDie + " + " + toHitBonus + "]]}} {{Damage=[[" + damageString + 
+            "]]}} " + criticalString);
     }
 });
