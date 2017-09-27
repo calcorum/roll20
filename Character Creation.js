8224fe76a517fbce713f693e9a0e1e3ce93cbd83
Cal Corum <calcorum@users.noreply.github.com>
2017-09-27 13:53:36 -0500

    Added grenade templates

--- Character Creation.js
+++ Character Creation.js
@@ -8,11 +8,14 @@ let skills = ["skill-acrobatics", "skill-athletics", "skill-bluff",
 let saves = ["save-fortitude", "save-reflex", "save-will"];
 let stats = ["hitpoints", "stamina", "resolve", "initiative",
     "profession00-name", "profession00-attribute", "profession01-name",
-    "profession00-attribute", "languages", "alignment", "size", "race"]
+    "profession00-attribute", "languages", "alignment", "size", "race",
+    "baseattackbonus"]
 let attributes = ["attribute-strength", "attribute-dexterity", 
     "attribute-constitution", "attribute-intelligence",
     "attribute-wisdom", "attribute-charisma"];
-let weaponExample = "/w gm &{template:default} {{name=@{character_name} / Tactical Semi-Auto Pistol}} {{To Hit=[[1d20+2]]}} {{Damage=[[1d6]] F; critical burn [[1d4]]}} {{Effect=None}}";
+let npchelpers = ["damagebonus","npc"]
+let weaponExample = "!fireweapon @{character_name} --laserpistolazimuth";
+let naturalWeaponExample = "/w gm &{template:default} {{name=Bite Attack}} {{To Hit=[[1d20+6]]}} {{Damage=[[1d6+3]] S plus [[1d3]] So}} {{Effect=None}}"
 let grenadeExample = "/w gm &{template:default} {{name=@{character_name} / Name of Grenade}} {{To Hit=[[1d20-?{Range Penalty? (-2 per range increment over 30ft)|0}]]; DC 5}} {{Damage=[[1d6]] P}} {{Effect=Explode 15 ft.}} {{Scatter=[Roll Here](!#ThrownWeapon-Scatter)}}";
 let spellExample = "/w gm &{template:default} {{name=@{character_name} / Spell Name}} {{Target=one creature}} {{Duration=instantaneous}} {{Saving Throw=Will half; DC [[10+SpellLevel+floor(((@{selected|attribute-intelligence}-10)/2))]]}} {{Spell Resistance=yes}} {{Damage=[[1d10]]}} {{Description=Any extra description needed}}"
 let notes = "/w gm &{template:default} {{name=@{character_name} / Notes}}{{Alignment / Race=@{alignment} / @{race}}} {{Speed=30 ft.}} {{Languages=@{languages}}} {{Before Combat=SampleText}} {{During Combat=SampleText}} {{Morale=SampleText}} {{Gear=AllTheLoot}}"
@@ -52,10 +55,20 @@ on("ready", function(){
                 name: "npc",
                 current: "yes",
                 characterid: char.id
-            });
+        });
+        createObj("attribute", {
+                name: "damagebonus",
+                current: "1",
+                characterid: char.id
+        });
         createObj("ability", {
             name: "Attack-Weapon-Sample",
             action: weaponExample,
+            characterid: char.id,
+        });
+        createObj("ability", {
+            name: "Attack-NaturalWeapon-Sample",
+            action: naturalWeaponExample,
             characterid: char.id,
         });
         createObj("ability", {
