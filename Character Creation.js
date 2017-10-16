94935a84958b822e23f6bc56af6028314fe1510f
Cal Corum <calcorum@users.noreply.github.com>
2017-10-16 00:06:56 -0500

    Updated defaults

--- Character Creation.js
+++ Character Creation.js
@@ -14,9 +14,9 @@ let attributes = ["attribute-strength", "attribute-dexterity",
     "attribute-constitution", "attribute-intelligence",
     "attribute-wisdom", "attribute-charisma"];
 let npchelpers = ["damagebonus","npc"]
-let weaponExample = "!fireweapon @{character_name} --laserpistolazimuth";
+let weaponExample = "!useweapon laserpistolazimuth";
 let naturalWeaponExample = "/w gm &{template:default} {{name=Bite Attack}} {{To Hit=[[1d20+6]]}} {{Damage=[[1d6+3]] S plus [[1d3]] So}} {{Effect=None}}"
-let grenadeExample = "/w gm &{template:default} {{name=@{character_name} / Name of Grenade}} {{To Hit=[[1d20-?{Range Penalty? (-2 per range increment over 30ft)|0}]]; DC 5}} {{Damage=[[1d6]] P}} {{Effect=Explode 15 ft.}} {{Scatter=[Roll Here](!#ThrownWeapon-Scatter)}}";
+let grenadeExample = "!throwgrenade fraggrenade1";
 let spellExample = "/w gm &{template:default} {{name=@{character_name} / Spell Name}} {{Target=one creature}} {{Duration=instantaneous}} {{Saving Throw=Will half; DC [[10+SpellLevel+floor(((@{selected|attribute-intelligence}-10)/2))]]}} {{Spell Resistance=yes}} {{Damage=[[1d10]]}} {{Description=Any extra description needed}}"
 let notes = "/w gm &{template:default} {{name=@{character_name} / Notes}}{{Alignment / Race=@{alignment} / @{race}}} {{Speed=30 ft.}} {{Languages=@{languages}}} {{Before Combat=SampleText}} {{During Combat=SampleText}} {{Morale=SampleText}} {{Gear=AllTheLoot}}"
 
@@ -83,7 +83,7 @@ on("ready", function(){
         });
         createObj("ability", {
             name: "Initiative",
-            action: "#Initiative",
+            action: "#Initiative-Enemy",
             istokenaction: true,
             characterid: char.id,
         });
