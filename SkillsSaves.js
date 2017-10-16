8c8e0b9d8ffb965eecf2e54ac2a369993fa61c76
Cal Corum <calcorum@users.noreply.github.com>
2017-10-15 22:01:44 -0500

    Added saves

--- SkillsSaves.js
+++ SkillsSaves.js
@@ -7,6 +7,7 @@ let skillList = {"acrobatics":"dexterity", "athletics":"strength",
     "sensemotive":"wisdom", "sleightofhand":"dexterity", "stealth":"dexterity", 
     "survival":"wisdom",
 };
+let saveList = {"fortitude":"constitution", "reflex":"dexterity", "will":"wisdom"}
 
 function getChar(charName){
     let character = findObjs({
@@ -40,14 +41,20 @@ function sendMessage(from, to, msg){
 function errorBadSkill(input, character){
     sendMessage(getChar("Clippy"), character, "Hi there! It looks like you're " + 
         "trying to roll a skill, but you must be too stupid to get the syntax " + 
-        "correct! Your skill input was: '" + input + "', but that ain't a skill.")
+        "correct! Your skill input was: '" + input + "', but that ain't a skill.");
+}
+
+function errorBadSave(input, character){
+    sendMessage(getChar("Clippy"), character, "Hi there! It looks like you're " + 
+        "trying to make a saving throw, but you must be too stupid to get the syntax " + 
+        "correct! Your save input was: '" + input + "', but that ain't a save.");
 }
 
 function capitalizeFirst(string){
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-function fixSkillName(skill){
+function fixName(skill){
     switch(skill){
         case "lifescience":
             return "Life Science";
@@ -64,7 +71,12 @@ function fixSkillName(skill){
 
 function rollCheck(char, skill, bonus){
     sendMessage(char, null, "&{template:default} {{name=" + char.get("name") + "}} {{" +
-        fixSkillName(skill) + " Check=[[1d20+" + bonus + "]]}}")
+        fixName(skill) + " Check=[[1d20+" + bonus + "]]}}")
+}
+
+function rollSave(char, save, bonus){
+    sendMessage(char, null, "&{template:default} {{name=" + char.get("name") + "}} {{" +
+        capitalizeFirst(save) + " Save=[[1d20+" + bonus + "]]}}")
 }
 
 // Syntax: !skill [SKILL_NAME] [optional modifier]
@@ -127,5 +139,58 @@ on("chat:message", function(msg){
         log("Skills / chatBonus: " + chatBonus);
         
         rollCheck(char, skillName, skBonus + skRanks + attBonus + chatBonus);
+    }else if(msg.content.indexOf("!save ") !== -1){
+        let char = getChar(msg.who);
+        if(char == null) {
+            sendMessage(getChar("Clippy"), null, "Who just tried to make a saving throw?" +
+                " I don't know youuuuuuu!");
+            return;
+        }
+        let chatBonus = 0, attBonus = 0, baseSave = 0, miscBonus = 0; 
+        let saveName = "";
+        let rawInput = msg.content.replace("!save ","");
+        // Make sure there is something listed where the save should be
+        if(rawInput.length <= 0) return;
+        let input = rawInput.split(" ");
+        saveName = input[0];
+        // Make sure what is listed first is a save
+        if(saveList[saveName] == undefined){
+            errorBadSave(saveName, char);
+            return;
+        }
+        // Get attribute bonus
+        attBonus = parseInt(Math.floor(getAttrByName(char.get("id"), "attribute-" + saveList[saveName])-10)/2);
+        if(isNaN(attBonus)) skBonus = 0;
+        // Get base save
+        baseSave = parseInt(getAttrByName(char.get("id"), "save-" + saveName, "current"));
+        if(isNaN(baseSave)) baseSave = 0;
+        // Get misc bonus
+        miscBonus = parseInt(getAttrByName(char.get("id"), "save-" + saveName, "max"));
+        if(isNaN(miscBonus)) miscBonus = 0;
+        // Get chat bonus
+        if(input[1] != undefined){
+            intSubString = parseInt(input[1].slice(1));
+            if(input[1].charAt(0) == '+' ){
+                if(intSubString){
+                    chatBonus = intSubString;
+                }
+            }else if(input[1].charAt(0) == '-'){
+                if(intSubString){
+                    chatBonus = -intSubString;
+                }
+            }else{
+                sendMessage(getChar("Clippy"), char, "Yo - imma throw this save for you, " +
+                    "but I'm not including the bonus you typed. I don't recognize this: " + 
+                    input[1]);
+            }
+        }
+        
+        log("Saves / Character: " + char.get("name"));
+        log("Saves / attBonus: " + attBonus);
+        log("Saves / baseSave: " + baseSave);
+        log("Saves / miscBonus: " + miscBonus);
+        log("Saves / chatBonus: " + chatBonus);
+        
+        rollSave(char, saveName, attBonus + baseSave + miscBonus + chatBonus);
     }
 });
