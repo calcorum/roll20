fe3f41f59d48d639f598addc7ca4c8a4123bc120
Cal Corum <calcorum@users.noreply.github.com>
2018-01-29 08:48:40 -0600

    Update SkillsSaves.js

--- SkillsSaves.js
+++ SkillsSaves.js
@@ -1,14 +1,30 @@
-const CHARLOG = false;
-let skillList = {"acrobatics":"dexterity", "athletics":"strength", 
-    "bluff":"charisma", "computers":"intelligence", "culture":"intelligence", 
-    "diplomacy":"charisma", "disguise":"charisma", "engineering":"intelligence", 
-    "intimidate":"charisma", "lifescience":"intelligence", "medicine":"intelligence", 
-    "mysticism":"wisdom", "perception":"wisdom", "physicalscience":"intelligence", 
-    "piloting":"dexterity", "profession00":"unknown", "profession01":"unknown", 
-    "sensemotive":"wisdom", "sleightofhand":"dexterity", "stealth":"dexterity", 
-    "survival":"wisdom",
+/*jshint esversion: 6 */
+
+const CHARLOG = true;
+let skillList = {
+    "acrobatics":{"attribute":"dexterity","acp":true}, 
+    "athletics":{"attribute":"strength","acp":true}, 
+    "bluff":{"attribute":"charisma","acp":false}, 
+    "computers":{"attribute":"intelligence","acp":false}, 
+    "culture":{"attribute":"intelligence","acp":false}, 
+    "diplomacy":{"attribute":"charisma","acp":false}, 
+    "disguise":{"attribute":"charisma","acp":false}, 
+    "engineering":{"attribute":"intelligence","acp":false}, 
+    "intimidate":{"attribute":"charisma","acp":false}, 
+    "lifescience":{"attribute":"intelligence","acp":false}, 
+    "medicine":{"attribute":"intelligence","acp":false}, 
+    "mysticism":{"attribute":"wisdom","acp":false}, 
+    "perception":{"attribute":"wisdom","acp":false}, 
+    "physicalscience":{"attribute":"intelligence","acp":false}, 
+    "piloting":{"attribute":"dexterity","acp":false}, 
+    "profession00":{"attribute":"unknown","acp":false}, 
+    "profession01":{"attribute":"unknown","acp":false}, 
+    "sensemotive":{"attribute":"wisdom","acp":false}, 
+    "sleightofhand":{"attribute":"dexterity","acp":true}, 
+    "stealth":{"attribute":"dexterity","acp":true}, 
+    "survival":{"attribute":"wisdom","acp":false},
 };
-let saveList = {"fortitude":"constitution", "reflex":"dexterity", "will":"wisdom"}
+let saveList = {"fortitude":"constitution", "reflex":"dexterity", "will":"wisdom"};
 
 function errorBadSkill(input, character){
     sendMessage(getChar("Clippy"), character, "Hi there! It looks like you're " + 
@@ -22,16 +38,18 @@ function errorBadSave(input, character){
         "correct! Your save input was: '" + input + "', but that ain't a save.");
 }
 
-function sendSkillDetailsToChar(char, skName, skBonus, skRanks, attBonus, chatBonus){
-    sendMessage(getChar("Clippy"), char, "&{template:default} {{name=" + char.get("name") + " " +
+function sendSkillDetailsToChar(char, skName, skBonus, skRanks, attBonus, chatBonus, acp){
+    sendMessage(getChar("Clippy"), char, "&{template:default} {{name=" + char.get("name") + " / " +
         fixName(skName) + " Log}} {{Skill Ranks=" + skRanks + "}} {{Skill Bonus=" + skBonus + "}} {{" +
-        "Attribute Bonus=" + attBonus + "}} {{Chat Bonus=" + chatBonus + "}}");
+        "Attribute Bonus=" + attBonus + "}} {{Chat Bonus=" + chatBonus + "}} {{Armor Check Penalty=" + acp + 
+        "}} {{Total Bonus=" + parseInt((skBonus) + (skRanks) + (attBonus) + (chatBonus) - (acp)) + "}}");
 }
 
 function sendSaveDetailsToChar(char, saveName, baseSave, miscBonus, attBonus, chatBonus){
-    sendMessage(getChar("Clippy"), char, "&{template:default} {{name=" + char.get("name") + " " +
-        capitalizeFirst(saveName) + " Log}} {{Base Save=" + baseSave + "}} {{Attribute Bonus=" + attBonus + "}} {{" +
-        "Misc Bonus=" + miscBonus + "}} {{Chat Bonus=" + chatBonus + "}}");
+    sendMessage(getChar("Clippy"), char, "&{template:default} {{name=" + char.get("name") + " / " +
+        fixName(saveName) + " Log}} {{Base Save=" + baseSave + "}} {{Attribute Bonus=" + attBonus + "}} {{" +
+        "Misc Bonus=" + miscBonus + "}} {{Chat Bonus=" + chatBonus + "}} {{Total Bonus=" + 
+        parseInt((baseSave) + (miscBonus) + (attBonus) + (chatBonus)) + "}}");
 }
 
 function fixName(skill){
@@ -53,12 +71,12 @@ function rollCheck(char, skill, bonus, checkName){
     if(checkName.length < 1) checkName = "";
     else checkName = " / " + checkName;
     sendMessage(char, null, "&{template:default} {{name=" + char.get("name") + checkName + "}} {{" +
-        fixName(skill) + " Check=[[1d20+" + bonus + "]]}}")
+        fixName(skill) + " Check=[[1d20+" + bonus + "]]}}");
 }
 
 function rollSave(char, save, bonus){
     sendMessage(char, null, "&{template:default} {{name=" + char.get("name") + "}} {{" +
-        capitalizeFirst(save) + " Save=[[1d20+" + bonus + "]]}}")
+        capitalizeFirst(save) + " Save=[[1d20+" + bonus + "]]}}");
 }
 
 // Syntax: !skill [SKILL_NAME] [optional modifier]
@@ -74,7 +92,7 @@ on("chat:message", function(msg){
                 " I don't know youuuuuuu!");
             return;
         }
-        let chatBonus = 0, attBonus = 0, skRanks = 0, skBonus = 0;
+        let chatBonus = 0, attBonus = 0, skRanks = 0, skBonus = 0, acp = 0;
         let skillName = "", checkName = "";
         
         let rawInput = msg.content.replace("!skill ","");
@@ -82,11 +100,14 @@ on("chat:message", function(msg){
         if(rawInput.length <= 0) return;
         let input = rawInput.split(" ");
         skillName = input[0];
-        // Make sure what is listed first is a skill
-        if(!skillList[skillName]){
+        log("skillname: " + skillName);
+        let skill = skillList[skillName] || null;
+        log("acp: " + skill.acp);
+        /*// Make sure what is listed first is a skill
+        if(skill !== null){
             errorBadSkill(skillName, char);
             return;
-        }
+        }*/
         // Get skill bonus from attributes
         skBonus = getAttribute(char, "skill-" + skillName, "max");
         if(isNaN(skBonus)) skBonus = 0;
@@ -94,8 +115,11 @@ on("chat:message", function(msg){
         skRanks = getAttribute(char, "skill-" + skillName);
         if(isNaN(skRanks)) skRanks = 0;
         // get attribute bonus
-        attBonus = Math.floor((getAttribute(char, "attribute-" + skillList[skillName])-10)/2);
+        attBonus = Math.floor((getAttribute(char, "attribute-" + skill.attribute)-10)/2);
         if(isNaN(attBonus)) attBonus = 0;
+        // check for armor check penalty
+        if(skill.acp) acp = getAttribute(char, "armorcheckpenalty") || 0;
+
         // Check for bonus typed into chat
         _.each(input, function(param){
             if(param.indexOf("charname") === 0){
@@ -121,9 +145,10 @@ on("chat:message", function(msg){
         log("SkillsSaves / main / skRanks: " + skRanks);
         log("SkillsSaves / main / attBonus: " + attBonus);
         log("SkillsSaves / main / chatBonus: " + chatBonus);
+        log("SkillsSaves / main / acp: " + acp);
         
-        if(CHARLOG) sendSkillDetailsToChar(char, skillName, skBonus, skRanks, attBonus, chatBonus);
-        rollCheck(char, skillName, skBonus + skRanks + attBonus + chatBonus, checkName);
+        if(CHARLOG) sendSkillDetailsToChar(char, skillName, skBonus, skRanks, attBonus, chatBonus, acp);
+        rollCheck(char, skillName, skBonus + skRanks + attBonus + chatBonus - acp, checkName);
     }else if(msg.content.indexOf("!save ") !== -1){
         let char = getChar(msg.who);
         if(char == null) {
@@ -144,7 +169,7 @@ on("chat:message", function(msg){
             return;
         }
         // Get attribute bonus
-        attBonus = Math.floor(getAttribute(char, "attribute-" + saveList[saveName])-10)/2;
+        attBonus = Math.floor((getAttribute(char, "attribute-" + saveList[saveName])-10)/2);
         if(isNaN(attBonus)) skBonus = 0;
         // Get base save
         baseSave = getAttribute(char, "save-" + saveName);
